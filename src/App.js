49541caef400e0ccc8b6d2d08b471e89a4e68483
Jakka Karthik <karthik9j@gmail.com>
2025-05-08 17:29:42 +0530

    initial changes in app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in 10 days
+          Learn React in 7 days
         </a>
       </header>
     </div>
